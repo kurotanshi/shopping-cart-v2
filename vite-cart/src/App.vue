4c2be020d6d45eca6377692084d74e38cb92c4ac
Kuro Hsu <kurotanshi@gmail.com>
2021-05-05 01:44:51 +0800

    將 ItemList 列表拆分出去

--- vite-cart/src/App.vue
+++ vite-cart/src/App.vue
@@ -19,32 +19,7 @@
     </header>
 
     <section class="container mt-4">
-    <div class="items row">
-      <div
-        class="col-sm-2"
-        v-for="item in itemList"
-        :key="item.id"
-      >
-        <div
-          :data-product-id="item.id"
-          class="card"
-        >
-          <img
-            :src="`./images/${item.cover}`"
-            :alt="item.name"
-            class="card-img-top"
-          >
-          <div class="card-body">
-            <h5 class="card-title fs-6 fw-light">{{ item.name }}</h5>
-            <p class="price">${{ item.price }}</p>
-            <button
-              @click="addToCart(item)"
-              class="btn btn-sm btn-warning fw-light"
-            ><i class="fas fa-cat"></i></button>
-          </div>
-        </div>
-      </div>
-    </div>
+      <ItemList :itemList="itemList" @add="addToCart" />
       <hr>
 
       <section class="cart">
@@ -102,17 +77,20 @@
 
 <script>
 import { ref, computed } from 'vue';
+import ItemList from './components/item.vue';
 
 export default {
+  components: {
+    ItemList
+  },
   setup() {
     
     // 商品列表
     const itemList = ref([]);
-    
+
     fetch('./item.json')
       .then(res => res.json())
       .then(d => itemList.value = itemList.value.concat(d));
-
 
     // 購物車列表
     const cartList = ref([]);
--- vite-cart/src/components/item.vue
+++ vite-cart/src/components/item.vue
@@ -1,9 +1,45 @@
 <template>
-  <div>
-    
+<div class="items row">
+  <div
+    class="col-sm-2"
+    v-for="item in itemList"
+    :key="item.id">
+    <div
+      :data-product-id="item.id"
+      class="card"
+    >
+      <img
+        :src="`./images/${item.cover}`"
+        :alt="item.name"
+        class="card-img-top"
+      >
+      <div class="card-body">
+        <h5 class="card-title fs-6 fw-light">{{ item.name }}</h5>
+        <p class="price">${{ item.price }}</p>
+        <button
+          @click="addToCart(item)"
+          class="btn btn-sm btn-warning fw-light"
+        ><i class="fas fa-cat"></i></button>
+      </div>
+    </div>
   </div>
+</div>
 </template>
 
 <script>
+export default {
+  props: {
+    itemList: Array
+  },
+  setup(props, context) {
 
+    const addToCart = item => {
+      context.emit('add', item);
+    }
+
+    return {
+      addToCart
+    }
+  }
+}
 </script>
