e108653f3b042855cb1b6a9ecd80442aa538e5c0
Kuro Hsu <kurotanshi@gmail.com>
2021-05-05 01:25:01 +0800

    購物車功能

--- vite-cart/src/App.vue
+++ vite-cart/src/App.vue
@@ -38,6 +38,7 @@
             <h5 class="card-title fs-6 fw-light">{{ item.name }}</h5>
             <p class="price">${{ item.price }}</p>
             <button
+              @click="addToCart(item)"
               class="btn btn-sm btn-warning fw-light"
             ><i class="fas fa-cat"></i></button>
           </div>
@@ -59,12 +60,27 @@
             </tr>
           </thead>
           <tbody>
-            <tr>
-            <td>老大</td>
-            <td><input type="number" class="quantity" min="1" value="1"></td>
-            <td>$20</td>
-            <td>$20</td>
-            <td><button data-id="624946E" class="remove-item-btn btn btn-danger btn-sm"><i class="fas fa-trash-alt"></i></button></td>
+            <tr
+              v-for="c in cartList"
+              :key="c.id"
+            >
+              <td>{{ c.name }}</td>
+              <td>
+                <input
+                  type="number"
+                  min="1"
+                  class="quantity"
+                  :value="c.qty">
+                </td>
+              <td>${{ c.price }}</td>
+              <td>${{ c.price * c.qty }}</td>
+              <td><button
+                  @click="removeFromCart(c.id)"
+                  :data-id="c.id"
+                  class="remove-item-btn btn btn-danger btn-sm"
+                >
+                  <i class="fas fa-trash-alt"></i></button>
+              </td>
             </tr>
           </tbody>
           <tfoot>
@@ -87,14 +103,47 @@ import { ref } from 'vue';
 
 export default {
   setup() {
+    
+    // 商品列表
     const itemList = ref([]);
     
     fetch('./item.json')
       .then(res => res.json())
       .then(d => itemList.value = itemList.value.concat(d));
 
+
+    // 購物車列表
+    const cartList = ref([]);
+    
+    // 加入購物車
+    const addToCart = (item) => {
+      const idx = cartList.value.findIndex((d) => item.id === d.id);
+      if (idx > -1) {
+        cartList.value[idx].qty++;
+      } else {
+        cartList.value.push({
+          ...item,
+          qty: 1,
+        });
+      }
+    };
+
+    // 從購物車移除
+    const removeFromCart = (itemId) => {
+      cartList.value = cartList.value.filter((item) => item.id != itemId);
+    };
+
+    // 清除購物車
+    const cleanCart = () => {
+      cartList.value.length = 0
+    };
+
     return {
-      itemList
+      itemList,
+      cartList,
+      addToCart,
+      removeFromCart,
+      cleanCart
     }
   }
 }
